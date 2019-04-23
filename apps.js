7546c1c6518cd8c674fccbb6496260edb339c963
Zulfikar Skywalker <31248833+lzulfikar22@users.noreply.github.com>
2019-04-23 21:16:26 +0700

    Add files via upload

--- apps.js
+++ apps.js
@@ -4,12 +4,29 @@ const app = new Vue({
         min: 1,
         max: 10,
         number: 1,
-        number2: 1
+        number2: 1,
+        jumlah: 0,
+        score: 0,
+        isi: 0,
+        jadul : 0,
+        jawaban : 0,
+    },
+    watch: {
+        isi: "ketik"
     },
     methods: {
-
+        ketik: function () {
+            this.jawaban = this.isi
+        },
         // Untuk tombol random
         getInput: function () {
+            this.jumlah = this.number + this.number2
+            if (this.jadul == this.jumlah){
+                this.score = this.score + 5
+            } else {
+                this.score = this.score
+            }
+            this.jadul = this.jawaban
             this.getRandomNumber()
         },
 
@@ -21,6 +38,6 @@ const app = new Vue({
         // Fungsi untuk random
         generateNumber: function () {
             return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-        }
+        },
     }
 })
